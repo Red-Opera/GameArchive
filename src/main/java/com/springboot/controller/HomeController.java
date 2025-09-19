f6b3b8f8978817a818e9d940756366787d1fdf4c
RedOpera <showme.the1018@gmail.com>
2025-09-20 00:04:17 +0900

    구글 admob를 위한 app-ads 추가

--- src/main/java/com/springboot/controller/HomeController.java
+++ src/main/java/com/springboot/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.springboot.model.VisitorCount;
+import org.springframework.http.ResponseEntity;
 
 @Controller
 public class HomeController 
@@ -32,5 +33,15 @@ public class HomeController
         model.addAttribute("message", "Privacy Policy");
         
         return "policy/PrivacyPolicy";
+    }
+
+    @GetMapping("/app-ads.txt")
+    public ResponseEntity<String> appAdsTxt() 
+    {
+        String content = "google.com, pub-4599880166858776, DIRECT, f08c47fec0942fa0";
+        
+        return ResponseEntity.ok()
+                .header("Content-Type", "text/plain; charset=utf-8")
+                .body(content);
     }
 }
